11aa49611b8deddbd9f42337ec1045df89706e68
grem62 <gremontmatheo22@gmail.com>
2023-05-15 15:46:00 +0200

    update betty

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,25 +10,25 @@
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
-	
+
 	/*verifie si le prt existe*/
 	if (!parent)
 		return (NULL);
-	
+
 	/*creation d un nvo noeud avc la value*/
 	new_node = binary_tree_node(parent, value);
 
 	/*si alloc memoire echec return null*/
 	if (!new_node)
 		return (NULL);
-	
+
 	/*si prt a deja un enfant droit, le deplacer à droite*/
 	if (parent->right != NULL)
 	{
 		new_node->right = parent->right;
 		new_node->right->parent = new_node;
-    	}	
-	
+		}
+
 	parent->right = new_node;
 
 
