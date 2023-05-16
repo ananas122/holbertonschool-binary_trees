b27625e2e1f6dc547ceb3a33485b0a83816bf62a
ananas122 <melle.noraagmail.com>
2023-05-16 07:32:13 -0700

    Correction Betty trailing whitespace and line 80 character

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,12 +8,13 @@
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t count = 0;
-	
+
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left != NULL || tree->right != NULL)
 	count++;
-	count = count + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
+	count = count + binary_tree_nodes(tree->left) +
+		binary_tree_nodes(tree->right);
 	return (count);
 }
