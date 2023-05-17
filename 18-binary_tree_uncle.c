2b2db860d9f32643671090d545b6ae5c912fbf42
grem62 <gremontmatheo22@gmail.com>
2023-05-17 11:12:16 +0200

    change the file uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -7,28 +7,27 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    // Vérifier si le nœud est NULL
-    if (node == NULL)
-    {
-        return NULL;
-    }
+	if (node == NULL)
+	{
+		return (NULL);
+	}
 
-    /*si le nœud n'a pas de parent ou que le parent n'a pas de parent*/
-    if (node->parent == NULL || node->parent->parent == NULL)
-    {
-        return NULL;
-    }
+	/*si le nœud n'a pas de parent ou que le parent n'a pas de parent*/
+	if (node->parent == NULL || node->parent->parent == NULL)
+	{
+		return (NULL);
+	}
 
-    /*Vérifier si le node est le FG ou D */
-    if (node->parent->parent->left == node->parent)
-    {
-        /* Return FD du parent qui a un parent*/
-        return node->parent->parent->right;
-    }
-    else
-    {
-        /* Return FG du grand parent*/
-        return node->parent->parent->left;
-    }
+	/*Vérifier si le node est le FG ou D */
+	if (node->parent->parent->left == node->parent)
+	{
+		/* Return FD du parent qui a un parent*/
+		return (node->parent->parent->right);
+	}
+	else
+	{
+		/* Return FG du grand parent*/
+		return (node->parent->parent->left);
+	}
 }
 
