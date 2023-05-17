d5c5064ef639c886dd5e1ad1d59d732de71cde26
ananas122 <melle.noraagmail.com>
2023-05-17 01:33:17 -0700

    Corrections height tree compilation

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,4 +1,4 @@
-#include "binary_trees"
+#include "binary_trees.h"
 /**
  * binary_tree_height - measures the height of a binary tree
  * @tree: a pointer to the root node of the tree to measure the height
