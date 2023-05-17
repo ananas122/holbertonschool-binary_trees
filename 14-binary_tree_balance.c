36036249201e83fe27d050628b478f54ada08861
ananas122 <melle.noraagmail.com>
2023-05-17 01:21:54 -0700

    Corrections

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_balance - function that measures the balance factor
  * @tree: pointer to the root node of the tree to measure the balance factor.
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
