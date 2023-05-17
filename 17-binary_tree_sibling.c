5e0e0574e593ef81432c1bdc00e27bfe2407cd5c
ananas122 <melle.noraagmail.com>
2023-05-17 03:36:33 -0700

    compilation after

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -3,7 +3,7 @@
 /**
  *binary_tree_sibling - function sibling brother of a node
  *@node: pointeur to a node.
- *Return: the brother left or right 
+ *Return: the brother left or right. 
 */
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
